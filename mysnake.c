7957ba4b6e100ff07f2813034642e56e6893c3d6
Krishna <khanchandani2904@gmail.com>
2025-08-20 15:14:36 +0530

    Completed

--- mysnake.c
+++ mysnake.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
+#include <windows.h>
 #define cols 25
 #define rows 25
 
@@ -9,6 +10,12 @@ char board[rows][cols];
 int isgameover=0;
 int eaten=1;
 int snake_len=1;
+int dx=1,dy=0;
+//put the snake head in center
+int posx= 12;
+int posy= 12;
+int a[256][2];
+
 
 //drew the board
 void draw_board()
@@ -33,17 +40,17 @@ void clear_screen()
     system("cls");
 }
 
-//put the snake head in center
-int posx= 12;
-int posy= 12;
-int a[256][2];
+
 
 void snake()
 {
-    char snake='@';
+    //a--->keeps coordinate of snake body
     a[0][0]=posx;
     a[0][1]=posy;
+    char snake='@';
+    char body='O';
     
+    //updates the new coordintate 
     for(int i=snake_len;i>0;i--)
     {
         a[i][0]=a[i-1][0];
@@ -51,7 +58,9 @@ void snake()
     }
     for(int k=snake_len;k>0;k--)
     {
+        if(k==1)
         board[a[k][0]][a[k][1]]=snake;
+        else board[a[k][0]][a[k][1]]=body;
     }
     
 
@@ -64,7 +73,7 @@ void print_board()
     {
         for(int j=0;j<rows;j++)
         {
-           printf("%c",board[i][j]);
+        printf("%c",board[i][j]);
         }
         printf("\n");
     } 
@@ -79,21 +88,23 @@ void transform(int x,int y)
 }
 
 //takes input
-void input()
-{
-    char m=getch();
-    switch (m)
-    {
-    case 'w':
-        transform(-1,0);
-        break;
-    case 's':
-        transform(1,0);break;
-    case 'a':
-        transform(0,-1);break;
-    case 'd':
-        transform(0,1);break;
-
+void input() {
+    if(_kbhit()) { // Only process input if key is pressed
+        char m = _getch();
+        switch (m) {
+            case 'w':
+                if(dx != 1||snake_len==1) { dx = -1; dy = 0; } // Prevent reverse direction
+                break;
+            case 's':
+                if(dx != -1||snake_len==1) { dx = 1; dy = 0; }
+                break;
+            case 'a':
+                if(dy != 1||snake_len==1) { dx = 0; dy = -1; }
+                break;
+            case 'd':
+                if(dy != -1||snake_len==1) { dx = 0; dy = 1; }
+                break;
+        }
     }
 }
 
@@ -103,12 +114,17 @@ void collison()
     {
         isgameover=1;
     }
+    for (int i = 1; i < snake_len; i++) {
+        if (posx == a[i][0] && posy == a[i][1]) {
+            isgameover = 1;
+        }
+    }
 
 }
- int x,y;
+int x,y;
 void food()
 {
-   
+
     if(eaten)
     {
         int max=23,min=1;
@@ -134,24 +150,35 @@ void Eaten()
 
 int main()
 {   
+    int difficulty=0;
+    int s=0;
+    printf("Choose Difficulty(1 to 3):\n");
+    scanf("%d",&difficulty);
+    if(difficulty==1) s=200;
+    if (difficulty==2) s=100;
+    if(difficulty==3) s=50;
     srand(time(NULL));
-    
+
     while(!isgameover)
     {
         clear_screen();
         draw_board();
         snake();
         food();
+        transform(dx,dy);
         print_board();
         input();
         Eaten(); 
+        Sleep(s);
         collison();
+        if(snake_len==255)
+        {
+            printf("You won");
+            return 0;
+        }
         
-        
-        
-        
-
     }
+    
     printf("Game Over");
     
 
